cb25165b64a0932b1bf85b7110bd6eceb8573884
xiao-zetian <1546879487@qq.com>
2020-02-13 17:33:35 +0800

    拼多多的包 和一个首页的接口

---
+++ src/com/keji09/erp/api/controller/IndexController.java
@@ -0,0 +1,49 @@
+package com.keji09.erp.api.controller;
+
+import com.keji09.erp.model.support.XDAOSupport;
+import com.keji09.erp.utils.Constants;
+import com.pdd.pop.sdk.common.util.JsonUtil;
+import com.pdd.pop.sdk.http.PopHttpClient;
+import com.pdd.pop.sdk.http.api.request.PddDdkGoodsSearchRequest;
+import com.pdd.pop.sdk.http.api.response.PddDdkGoodsSearchResponse;
+import com.sun.org.apache.xpath.internal.objects.XObject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * 公网静态页面调用API控制器
+ * 
+ */
+@Controller
+@RequestMapping("/app_index")
+public class IndexController extends XDAOSupport {
+
+	/**
+	 * 输出验证码
+	 */
+	@RequestMapping(method = RequestMethod.GET)
+	@ResponseBody
+	public Object getCaptcha(HttpServletRequest req, HttpServletResponse resp
+		, ModelMap map) {
+		String clientId = Constants.CLIENT_ID;
+		String clientSecret = Constants.CLIENT_SECRET;
+		PopHttpClient client = new PopHttpClient(clientId, clientSecret);
+		PddDdkGoodsSearchRequest request = new PddDdkGoodsSearchRequest();
+		try {
+			PddDdkGoodsSearchResponse response = client.syncInvoke(request);
+			System.out.println(JsonUtil.transferToJson(response));
+			return  response;
+			//msg+=JsonUtil.transferToJson(response);
+		}catch (Exception e){
+		}
+		return map;
+	}
+
+}
